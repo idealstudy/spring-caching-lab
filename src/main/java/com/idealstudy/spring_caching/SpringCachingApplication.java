2e9151b1292ed3a4d2a78140e34c188928da207d
jin66 <dbf6609@naver.com>
2024-09-06 22:58:58 +0900

    init

---
+++ src/main/java/com/idealstudy/spring_caching/SpringCachingApplication.java
@@ -0,0 +1,13 @@
+package com.idealstudy.spring_caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idealstudy/spring_caching/SpringCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idealstudy.spring_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
